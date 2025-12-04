10f0deaba732b00f8d78ce649cfdfdd380c7f5b1
priyanshu7175 <priyanshuadhikari7@gmail.com>
2025-12-04 03:38:06 -0500

    Creating a Spring Boot project Secventory

---
+++ src/main/java/com/example/Secventory/SecventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Secventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Secventory/SecventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Secventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
